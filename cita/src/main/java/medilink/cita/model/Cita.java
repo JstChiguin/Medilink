0f0cbd75eaff1355de7c0ebb8d9a1879f0303004
JstChiguin <ch.chiang@duocuc.cl>
2026-04-24 20:49:56 -0400

    commit inicial

---
+++ cita/src/main/java/medilink/cita/model/Cita.java
@@ -0,0 +1,26 @@
+package medilink.cita.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Cita {
+    private Long idCita; //PK
+    private Long idPaciente; //FK
+    private Long idProfesional; //FK
+    private Long idAgenda; //FK
+    private Date fechaCita;
+    private Time horaCita;
+    private String modalidadAtencionCita;
+    private String estadoCita;
+    private String motivoCita;
+    private String observacionesCita;
+    private Date fechaCreacionCita;
+}
