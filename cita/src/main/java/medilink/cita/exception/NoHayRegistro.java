7ab685a5a3d123f266faa7f2e76a88c747d3518a
JstChiguin <ch.chiang@duocuc.cl>
2026-05-14 15:50:19 -0400

    Delete cita directory

--- cita/src/main/java/medilink/cita/exception/NoHayRegistro.java
+++
@@ -1,7 +0,0 @@
-package medilink.cita.exception;
-
-public class NoHayRegistro extends RuntimeException {
-    public NoHayRegistro(String message) {
-        super(message);
-    }
-}
--- cita/src/main/java/medilink/cita/repository/CitaRepository.java
+++
@@ -1,7 +0,0 @@
-package medilink.cita.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CitaRepository {
-}
