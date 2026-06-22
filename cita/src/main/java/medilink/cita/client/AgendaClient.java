e44fc50a51290497c2b9a07378e5ef193c3b16fc
JstChiguin <ch.chiang@duocuc.cl>
2026-06-22 18:23:53 -0400

    arreglo
    arreglo

--- cita/src/main/java/medilink/cita/client/AgendaClient.java
+++ cita/src/main/java/medilink/cita/client/AgendaClient.java
@@ -4,7 +4,7 @@ import medilink.cita.dto.response.AgendaResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "agenda-service", path = "/api/v1/agendas")
+@FeignClient(name = "agenda-medica", path = "/api/v1/agendas")
 public interface AgendaClient {
 
     @GetMapping("/{idAgenda}")
