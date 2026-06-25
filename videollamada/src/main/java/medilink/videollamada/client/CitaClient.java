26cbed6bfdd3ee824c67078ef022147ddb947c90
JstChiguin <ch.chiang@duocuc.cl>
2026-06-24 21:42:45 -0400

    Corrección comunicación entre microservicios
    - Se unificó el nombre del servicio Citas en OpenFeign.
    - Se corrigieron los clientes Feign para utilizar "citas".
    - Se ajustó la configuración del microservicio Agenda Médica.
    - Se validó la comunicación entre Agenda, Citas, Pagos, Historial, Recetas, Evaluación, Notificación y Videollamada mediante Postman.

--- videollamada/src/main/java/medilink/videollamada/client/CitaClient.java
+++ videollamada/src/main/java/medilink/videollamada/client/CitaClient.java
@@ -4,7 +4,7 @@ import medilink.videollamada.dto.response.CitaResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "cita-service", path = "/api/v1/citas")
+@FeignClient(name = "citas", path = "/api/v1/citas")
 public interface CitaClient {
 
     @GetMapping("/{idCita}")
